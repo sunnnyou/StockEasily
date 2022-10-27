1275c81816a6a966be3622c5982aff5ed0cb22d5
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-10-28 01:55:48 +0200

    Add StockEasily-API spring boot template project with mariadb driver and spring web

---
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/StockEasilyApiApplication.java
@@ -0,0 +1,13 @@
+package de.throsenheim.unlimited.stockeasilyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockEasilyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockEasilyApiApplication.class, args);
+    }
+
+}
---
+++ StockEasily-API/src/test/java/de/throsenheim/unlimited/stockeasilyapi/StockEasilyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.throsenheim.unlimited.stockeasilyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockEasilyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
