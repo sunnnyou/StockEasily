85c13117d2839172c3c8bb388334b3b7ca795ad9
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-01 21:11:56 +0100

    Add ArticlesController

---
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/controller/ArticlesController.java
@@ -0,0 +1,29 @@
+package de.throsenheim.unlimited.stockeasilyapi.controller;
+
+import de.throsenheim.unlimited.stockeasilyapi.dto.ArticleCreationDto;
+import de.throsenheim.unlimited.stockeasilyapi.model.Article;
+import de.throsenheim.unlimited.stockeasilyapi.service.IArticlesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+public class ArticlesController {
+
+    private final IArticlesService articleService;
+
+    @Autowired
+    public ArticlesController(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") IArticlesService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping
+    Article createArticle(ArticleCreationDto creationDto) {
+        // this.articleService.
+        // TODO implement
+        return this.articleService.create(creationDto);
+    }
+
+}
