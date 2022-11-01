17ba15a514bc1d4e66197ff97d1c22be0170a861
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-02 00:22:14 +0100

    Add validation annotations

--- StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/dto/ArticleCreationDto.java
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/dto/ArticleCreationDto.java
@@ -2,11 +2,16 @@ package de.throsenheim.unlimited.stockeasilyapi.dto;
 
 import de.throsenheim.unlimited.stockeasilyapi.model.Property;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public class ArticleCreationDto {
 
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotNull(message = "Properties are mandatory")
     private List<Property> properties;
 
     public String getName() {
