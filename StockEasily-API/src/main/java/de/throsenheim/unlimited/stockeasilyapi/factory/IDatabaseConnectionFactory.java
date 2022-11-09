b489fd75f2f7120c81dd9619faabd82dddd11502
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-09 18:06:42 +0100

    Add DatabaseConnectionFactory

---
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/factory/IDatabaseConnectionFactory.java
@@ -0,0 +1,17 @@
+package de.throsenheim.unlimited.stockeasilyapi.factory;
+
+import org.springframework.core.env.Environment;
+
+import java.sql.Connection;
+
+public interface IDatabaseConnectionFactory {
+
+    Connection getConnection();
+
+    Connection getConnection(boolean autoCommit);
+
+    Connection getConnection(Environment environment);
+
+    Connection getConnection(Environment environment, boolean autoCommit);
+
+}
