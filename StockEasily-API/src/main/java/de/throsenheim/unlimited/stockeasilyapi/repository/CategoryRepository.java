335a89df8229fe7725478ab619f2609b35f6dc01
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-09 22:58:40 +0100

    Add CategoryRepository

---
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/repository/CategoryRepository.java
@@ -0,0 +1,60 @@
+package de.throsenheim.unlimited.stockeasilyapi.repository;
+
+import de.throsenheim.unlimited.stockeasilyapi.factory.IDatabaseConnectionFactory;
+import de.throsenheim.unlimited.stockeasilyapi.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@Component
+public class CategoryRepository implements HumaneRepository<Category, Long> {
+
+    private final Connection connection;
+
+    @Autowired
+    public CategoryRepository(IDatabaseConnectionFactory databaseConnectionFactory) {
+        this.connection = databaseConnectionFactory.getConnection(false);
+    }
+
+    @Override
+    public Iterable<Category> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public Optional<Category> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category findByName(String name) {
+        final String query = "SELECT id FROM categories WHERE name = ? LIMIT 1";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Category result = new Category();
+            result.setId(resultSet.getInt("id"));
+            result.setName(name);
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Iterable<Category> saveAll(Iterable<Category> entities) {
+        return null;
+    }
+
+    @Override
+    public Category save(Category entity) {
+        return null;
+    }
+
+}
