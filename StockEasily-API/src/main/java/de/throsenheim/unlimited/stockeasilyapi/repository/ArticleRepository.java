62a8a96f040d7c5f58fbc867e68c5e9ff6881013
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-09 23:00:05 +0100

    Implement HumaneRepository and start first approach to create article

--- StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/repository/ArticleRepository.java
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/repository/ArticleRepository.java
@@ -1,72 +1,48 @@
 package de.throsenheim.unlimited.stockeasilyapi.repository;
 
-import de.throsenheim.unlimited.stockeasilyapi.exception.NotImplementedException;
 import de.throsenheim.unlimited.stockeasilyapi.factory.IDatabaseConnectionFactory;
 import de.throsenheim.unlimited.stockeasilyapi.model.Article;
+import de.throsenheim.unlimited.stockeasilyapi.model.Category;
+import de.throsenheim.unlimited.stockeasilyapi.model.Property;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ArticleRepository implements CrudRepository<Article, Integer> {
+public class ArticleRepository implements HumaneRepository<Article, Long> {
 
     private final Connection connection;
+    private final PropertyRepository propertyRepository;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
-    public ArticleRepository(IDatabaseConnectionFactory databaseConnectionFactory) {
+    public ArticleRepository(IDatabaseConnectionFactory databaseConnectionFactory,
+                             PropertyRepository propertyRepository,
+                             CategoryRepository categoryRepository) {
         this.connection = databaseConnectionFactory.getConnection(false);
+        this.propertyRepository = propertyRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public long count() {
-        throw new NotImplementedException("ArticleRepository.count");
+    public Iterable<Article> findAllById(Iterable<Long> longs) {
+        return null;
     }
 
     @Override
-    public void deleteAll() {
-        throw new NotImplementedException("ArticleRepository.deleteAll");
+    public Optional<Article> findById(Long aLong) {
+        return Optional.empty();
     }
 
     @Override
-    public void deleteAll(Iterable<? extends Article> articles) {
-        throw new NotImplementedException("ArticleRepository.deleteAll");
+    public Article findByName(String name) {
+        return null;
     }
 
-    @Override
-    public void deleteAllById(Iterable<? extends Integer> ids) {
-        throw new NotImplementedException("ArticleRepository.deleteAllById");
-    }
-
-    @Override
-    public void delete(Article articles) {
-        throw new NotImplementedException("ArticleRepository.delete");
-    }
-
-    @Override
-    public void deleteById(Integer id) {
-        throw new NotImplementedException("ArticleRepository.deleteById");
-    }
-
-    @Override
-    public boolean existsById(Integer id) {
-        return findById(id).isPresent();
-    }
-
-    @Override
-    public Iterable<Article> findAll() {
-        throw new NotImplementedException("ArticleRepository.findAll");
-    }
-
-    @Override
-    public Iterable<Article> findAllById(Iterable<Integer> ids) {
-        throw new NotImplementedException("ArticleRepository.findAllById");
-    }
-
-    @Override
-    public Optional<Article> findById(Integer id) {
 ////        try {
 ////            String table = "";
 ////            final String query = "SELECT * FROM articles WHERE id = ? LIMIT 1";
@@ -81,16 +57,61 @@ public class ArticleRepository implements CrudRepository<Article, Integer> {
 ////        } catch (SQLException e) {
 ////            throw new RuntimeException(e);
 ////        }
-        return Optional.empty();
+
+    @Override
+    public Article save(Article article) {
+        Article result = insertArticle(article);
+        // TODO use id
+
+        Category category = article.getCategory();
+        if (category != null) {
+            category = categoryRepository.save(category);
+            article.setCategory(category);
+        }
+
+        List<Property> properties = article.getProperties();
+        if (properties != null) {
+            properties = (ArrayList<Property>) propertyRepository.saveAll(article.getProperties());
+            article.setProperties(properties);
+        }
+
+        return result;
     }
 
     @Override
-    public <S extends Article> S save(S article) {
+    public Iterable<Article> saveAll(Iterable<Article> articles) {
+        for (Article article : articles) {
+            save(article);
+        }
         return null;
     }
 
-    @Override
-    public <S extends Article> Iterable<S> saveAll(Iterable<S> articles) {
-        return null;
+    private Article insertArticle(Article article) {
+        try {
+            final String query = "INSERT INTO articles(name,quantity,image) VALUES (?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, article.getName());
+            preparedStatement.setInt(2, article.getQuantity());
+            preparedStatement.setBlob(3, article.getImage()); // TODO implement image upload
+
+            int success = preparedStatement.executeUpdate();
+            if (success == 1) {
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    article.setId(resultSet.getLong(1));
+                }
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertCategory() {
+
+    }
+
+    private void insertProperties() {
+
     }
 }
