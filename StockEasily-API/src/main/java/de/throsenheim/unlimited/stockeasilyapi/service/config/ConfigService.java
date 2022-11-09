a7f0c336e84bccd4216b1b934e8a35df47dc4bd3
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-09 22:44:09 +0100

    Remove unused Service annotation

--- StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/service/config/ConfigService.java
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/service/config/ConfigService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component
-@Service
 public class ConfigService implements IConfigService {
 
     private final Environment environment;
