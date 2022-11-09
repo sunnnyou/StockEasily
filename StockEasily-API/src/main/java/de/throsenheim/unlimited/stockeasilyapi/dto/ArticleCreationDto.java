1a1ebf0e1e48d8f29ca4b470c6bffdfc3d6ca84a
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-09 22:59:22 +0100

    Add members and its getters and setters, validate

--- StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/dto/ArticleCreationDto.java
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/dto/ArticleCreationDto.java
@@ -1,7 +1,11 @@
 package de.throsenheim.unlimited.stockeasilyapi.dto;
 
+import de.throsenheim.unlimited.stockeasilyapi.model.Category;
 import de.throsenheim.unlimited.stockeasilyapi.model.Property;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -9,25 +13,56 @@ import java.util.List;
 public class ArticleCreationDto {
 
     @NotBlank(message = "Name is mandatory")
+    @Length(min = 1, max = 30)
     private String name;
 
     @NotNull(message = "Properties are mandatory")
     private List<Property> properties;
 
+    private Category category;
+
+    @Min(value = 0)
+    private int quantity = 1;
+
+    private MultipartFile image; // TODO validate size
+
     public String getName() {
         return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public MultipartFile getImage() {
+        return image;
     }
 
     public List<Property> getProperties() {
         return properties;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
     }
 
     public void setProperties(List<Property> properties) {
         this.properties = properties;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
