7efd1568feeac6a1c89ec0ba91dedded7caefaa5
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-09 22:42:39 +0100

    Add generic HumaneRepository interface

---
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/repository/HumaneRepository.java
@@ -0,0 +1,35 @@
+package de.throsenheim.unlimited.stockeasilyapi.repository;
+
+import de.throsenheim.unlimited.stockeasilyapi.model.Article;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+
+public interface HumaneRepository<T, ID> extends Repository<T, ID> {
+
+    // Enable and implement when needed or throw new NotImplementedException otherwise
+
+//    ID count();
+
+//    void deleteAll();
+
+//    void deleteAll(Iterable<T> entities);
+
+//    void delete(T entity);
+
+//    void deleteById(ID id);
+
+//    boolean existsById(ID id);
+
+//    Iterable<T> findAll();
+
+    Iterable<T> findAllById(Iterable<ID> ids);
+
+    Optional<T> findById(ID id);
+
+    T findByName(String name);
+
+    Iterable<T> saveAll(Iterable<T> entities);
+
+    T save(T entity);
+}
