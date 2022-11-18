2b7102bf186018cbba668148cb0f2dbfc9bf56f4
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-18 19:45:22 +0100

    Add FaIcon component

---
+++ StockEasily-UI/src/components/common/FaIcon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Fa from 'svelte-fa/src/fa.svelte';
+
+    export let className = '';
+    export let fw = false;
+    export let icon: any;
+</script>
+
+<span class="inline">
+    <i class="inline-block {className}">
+        <Fa {icon} {fw}/>
+    </i>
+    <span> <slot/> </span>
+</span>
