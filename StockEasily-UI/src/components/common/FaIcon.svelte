95784ee7402eec9b0a6ddc3e5832b8e5916c7879
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 01:12:18 +0100

    Install and use svelte-fa dev dependency

--- StockEasily-UI/src/components/common/FaIcon.svelte
+++ StockEasily-UI/src/components/common/FaIcon.svelte
@@ -2,13 +2,31 @@
     import Fa from 'svelte-fa/src/fa.svelte';
 
     export let className = '';
+    export let slotClass = '';
     export let fw = false;
     export let icon: any;
 </script>
 
-<span class="inline">
+<span class="inline flex-container">
     <i class="inline-block {className}">
         <Fa {icon} {fw}/>
     </i>
-    <span> <slot/> </span>
+    <span class="pb-1 md:pb-0 text-sm{slotClass ? ' ' + slotClass : ''}">
+        <slot/>
+    </span>
 </span>
+
+<style>
+    .flex-container {
+        display: flex;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
+
+    .flex-container > i {
+        /*width: 100px;*/
+        /*margin: 10px;*/
+        text-align: center;
+        line-height: 20px;
+    }
+</style>
