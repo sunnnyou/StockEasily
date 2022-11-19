db85023e1b896780b29c7bf6632acc5bc167ec9a
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 03:29:42 +0100

    Use class conditionally

--- StockEasily-UI/src/components/template/menu/main/MainMenuButton.svelte
+++ StockEasily-UI/src/components/template/menu/main/MainMenuButton.svelte
@@ -12,12 +12,10 @@
         return $page.url.pathname === button?.href;
     }
 
-    const selectedClass = 'text-blue-500 border-blue-500 hover:border-blue-500';
-    const normalClass= ' hover:border-blue-500';
 </script>
 
 <A href={button?.href}
-   className="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 {isSelected() ? selectedClass: normalClass}">
+   className="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-gray-900 border-b-2 hover:border-blue-500 {isSelected() ? 'text-blue-500 border-blue-500' : 'text-gray-500'}">
     <FaIcon className="mr-3" icon={button?.icon} fw={true}>
         {#if button.i18n}
             {$t(button.i18n)}
