7cfe3a9849be73fc2bf73d830062d6d51991b277
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 02:57:15 +0100

    Add scale and color props

--- StockEasily-UI/src/components/common/FaIcon.svelte
+++ StockEasily-UI/src/components/common/FaIcon.svelte
@@ -5,11 +5,13 @@
     export let slotClass = '';
     export let fw = false;
     export let icon: any;
+    export let scale = 1;
+    export let color = '';
 </script>
 
 <span class="inline flex-container">
     <i class="inline-block {className}">
-        <Fa {icon} {fw}/>
+        <Fa {icon} {fw} {color} {scale}/>
     </i>
     <span class="pb-1 md:pb-0 text-sm{slotClass ? ' ' + slotClass : ''}">
         <slot/>
