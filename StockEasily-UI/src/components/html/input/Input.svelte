11225cc4a55f1481f7b0817fc1a537c68e8b2a11
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 02:55:49 +0100

    Add Input component

---
+++ StockEasily-UI/src/components/html/input/Input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {InputType} from './input-type';
+
+    export let className = '';
+    export let id = '';
+    export let name = '';
+    export let placeholder = '';
+    export let type = InputType.Text;
+    export let value = '';
+</script>
+
+<input {type} {name} {id} class={className} {placeholder} {value}>
