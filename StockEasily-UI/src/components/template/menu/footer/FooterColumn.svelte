df6872c1349ffb8e6fc2eed646168bbdceb589f0
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 02:50:59 +0100

    Add Footer component

---
+++ StockEasily-UI/src/components/template/menu/footer/FooterColumn.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import {ATarget} from '$components/html/a/a-target';
+    import {t} from '$i18n/i18n';
+
+    import A from '$components/html/a/A.svelte';
+
+    export let className = '';
+    export let parentClassName = '';
+    export let rows = [];
+
+    function getClassName() {
+        return className?.length > 0 ? '' + className : '';
+    }
+</script>
+
+<div class="px-8 inline-block{parentClassName?.length > 0 ? ' ' + parentClassName : ''}">
+    <ul class="list-reset items-center text-sm pt-3">
+        {#each rows as row}
+            <li>
+                <A href={row.url}
+                   className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-1{getClassName()}"
+                   target={ATarget.Blank}
+                >
+                    {$t(row.i18n)}
+                </A>
+            </li>
+        {/each}
+    </ul>
+</div>
