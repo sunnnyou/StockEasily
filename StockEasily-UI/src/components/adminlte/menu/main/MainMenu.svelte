ad9f2f5d6c84718afca965da12cf74967ec50698
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 02:27:28 +0100

    Add main menu

---
+++ StockEasily-UI/src/components/adminlte/menu/main/MainMenu.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import {faChartArea, faEnvelope, faHome, faTasks, faWallet} from '@fortawesome/free-solid-svg-icons';
+    import MainMenuButton from './MainMenuButton.svelte';
+
+    export let buttons = [
+        {
+            href: '/',
+            i18n: 'menu.home',
+            icon: faHome,
+        },
+        {
+            href: '/tasks',
+            i18n: 'menu.tasks',
+            icon: faTasks,
+        },
+        {
+            href: '/messages',
+            i18n: 'menu.messages',
+            icon: faEnvelope,
+        },
+        {
+            href: '/analytics',
+            i18n: 'menu.analytics',
+            icon: faChartArea,
+        },
+        {
+            href: '/payments',
+            i18n: 'menu.payments',
+            icon: faWallet,
+        },
+    ];
+</script>
+
+<ul class="list-reset lg:flex flex-1 items-center px-4 md:px-0">
+    {#each buttons as button}
+        <li class="mr-6 my-2 md:my-0">
+            <MainMenuButton {button}/>
+        </li>
+    {/each}
+
+
+<!--    <li class="mr-6 my-2 md:my-0">-->
+<!--        <a href="#"-->
+<!--           class="block py-1 md:py-3 pl-1 align-middle text-pink-600 no-underline hover:text-gray-900 border-b-2 border-orange-600 hover:border-orange-600">-->
+<!--            <i class="fas fa-home fa-fw mr-3 text-pink-600"></i><span-->
+<!--                class="pb-1 md:pb-0 text-sm">Home</span>-->
+<!--        </a>-->
+<!--    </li>-->
+<!--    <li class="mr-6 my-2 md:my-0">-->
+<!--        <a href="#"-->
+<!--           class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-pink-500">-->
+<!--            <i class="fas fa-tasks fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Tasks</span>-->
+<!--        </a>-->
+<!--    </li>-->
+<!--    <li class="mr-6 my-2 md:my-0">-->
+<!--        <a href="#"-->
+<!--           class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-purple-500">-->
+<!--            <i class="fa fa-envelope fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Messages</span>-->
+<!--        </a>-->
+<!--    </li>-->
+<!--    <li class="mr-6 my-2 md:my-0">-->
+<!--        <a href="#"-->
+<!--           class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-green-500">-->
+<!--            <i class="fas fa-chart-area fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Analytics</span>-->
+<!--        </a>-->
+<!--    </li>-->
+<!--    <li class="mr-6 my-2 md:my-0">-->
+<!--        <a href="#"-->
+<!--           class="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 border-white hover:border-red-500">-->
+<!--            <i class="fa fa-wallet fa-fw mr-3"></i><span class="pb-1 md:pb-0 text-sm">Payments</span>-->
+<!--        </a>-->
+<!--    </li>-->
+</ul>
---
+++ StockEasily-UI/src/components/adminlte/menu/main/MainMenuButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type {MenuButton} from '$common/menu-button';
+    import {t} from '$i18n/i18n';
+    import {page} from '$app/stores';
+
+    import A from '$components/html/a/A.svelte';
+    import FaIcon from '$components/common/FaIcon.svelte';
+
+    export let button: MenuButton | undefined;
+
+    function isSelected() {
+        return $page.url.pathname === button?.href;
+    }
+
+    const selectedClass = 'text-pink-600 border-pink-600 hover:border-pink-600';
+    const normalClass= ' hover:border-orange-600';
+</script>
+
+<A href={button?.href}
+   className="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-900 border-b-2 {isSelected() ? selectedClass: normalClass}">
+    <FaIcon className="mr-3" icon={button?.icon} fw={true}>
+        {#if button.i18n}
+            {$t(button.i18n)}
+        {/if}
+    </FaIcon>
+</A>
