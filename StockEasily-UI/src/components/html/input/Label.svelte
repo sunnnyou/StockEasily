a27543f70e5e59cad6ca36cfc3ed1b60fc8c2e4c
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-11-19 02:56:57 +0100

    Add Label component

---
+++ StockEasily-UI/src/components/html/input/Label.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let className = '';
+    export let id = '';
+    export let name = '';
+</script>
+
+<!--suppress XmlInvalidId -->
+<label for={name} {id} class={className}>
+    {#if $$slots.default}
+        <slot/>
+    {/if}
+</label>
