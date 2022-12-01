678a9666a2ccc572ab58057dbd05667d698b5d6f
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-12-01 23:57:30 +0100

    Move swagger config to config/SwaggerConfig

--- StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/StockEasilyApiApplication.java
+++ StockEasily-API/src/main/java/de/throsenheim/unlimited/stockeasilyapi/StockEasilyApiApplication.java
@@ -2,13 +2,6 @@ package de.throsenheim.unlimited.stockeasilyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class StockEasilyApiApplication {
@@ -16,24 +9,4 @@ public class StockEasilyApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockEasilyApiApplication.class, args);
     }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("de.throsenheim.unlimited"))
-                .paths(PathSelectors.regex("/api.*"))
-                .build()
-                .apiInfo(metadata());
-    }
-
-    private static ApiInfo metadata() {
-        return new ApiInfoBuilder()
-                .title("StockEasily")
-                .description("StockEasily is a software system for organizing your home and stuff.\n" +
-                        "It allows to search and manage your items with ease.")
-                .version("1.0")
-                .build();
-    }
-
 }
