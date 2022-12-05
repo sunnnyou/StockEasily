2ce6f3ad08d50e2cf93b7fa8a83338b6fdab9807
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-12-05 15:36:06 +0100

    Reorder props

--- StockEasily-UI/src/components/template/SearchField.svelte
+++ StockEasily-UI/src/components/template/SearchField.svelte
@@ -12,9 +12,9 @@
 <div class="relative pull-right md:pr-0 my-2 px-4"
      {title}
 >
-    <Input {placeholder}
+    <Input className="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal"
+           {placeholder}
            type={InputType.Search}
-           className="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal"
     />
     <div class="absolute search-icon left-7 top-3">
         <FaIcon icon={faMagnifyingGlass}/>
