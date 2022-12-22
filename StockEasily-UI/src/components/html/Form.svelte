b69930081f20686d013db7bda7de0646bace8acd
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2022-12-22 21:08:47 +0100

    Remove unneeded props from Form component and minimize it

--- StockEasily-UI/src/components/html/Form.svelte
+++ StockEasily-UI/src/components/html/Form.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
     export let className = '';
-    export let onSubmit: Function;
-
-    function handleSubmit() {
-    }
 </script>
 
 <form class={className}
-      on:submit|preventDefault={handleSubmit}
-      on:submit|once={() => onSubmit()}
+      on:submit|preventDefault
 >
     <slot/>
 </form>
