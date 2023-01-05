a5146b286eda3b70b28e9270b2fb035db2693caa
Sunny Reitgassl <sunny.reitgassl@stud.th-rosenheim.de>
2023-01-05 23:38:55 +0100

    formatted and added height and width to img

--- StockEasily-UI/src/routes/articles/page/[page]/+page.svelte
+++ StockEasily-UI/src/routes/articles/page/[page]/+page.svelte
@@ -63,7 +63,7 @@
                     beforePage = Number($page.params.page) - 1;
                     beforePage = '/articles/page/' + beforePage + '?' + pageQuery;
                     afterPage = Number($page.params.page) + 1;
-                    afterPage = '/articles/page/' + afterPage+ '?' + pageQuery;
+                    afterPage = '/articles/page/' + afterPage + '?' + pageQuery;
                     console.log(maxPage);
                 } else {
                     console.log(response.status)
@@ -108,27 +108,43 @@
 
                     <!-- Search Bar -->
                     <div class="p-4">
-                        <form on:submit|preventDefault={search} class="flex items-center">
-                            <label for="simple-search" class="sr-only">Search</label>
+                        <form on:submit|preventDefault={search}
+                              class="flex items-center">
+                            <label for="simple-search"
+                                   class="sr-only">
+                                Search
+                            </label>
                             <div class="relative w-full">
                                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <svg aria-hidden="true"
+                                         class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                         fill="currentColor"
+                                         viewBox="0 0 20 20"
+                                         xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd"
                                               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                              clip-rule="evenodd"></path>
+                                              clip-rule="evenodd">
+                                        </path>
                                     </svg>
                                 </div>
-                                <input bind:value={searchQuery} type="text" id="simple-search"
+                                <input bind:value={searchQuery}
+                                       type="text"
+                                       id="simple-search"
                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="Search" required>
+                                       placeholder="Search"
+                                       required>
                             </div>
                             <button type="submit"
                                     class="p-2.5 ml-2 text-sm font-medium text-white bg-gray-700 rounded-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                <svg class="w-5 h-5"
+                                     fill="none" stroke="currentColor"
+                                     viewBox="0 0 24 24"
                                      xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                    <path stroke-linecap="round"
+                                          stroke-linejoin="round"
+                                          stroke-width="2"
+                                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+                                    </path>
                                 </svg>
                                 <span class="sr-only">Search</span>
                             </button>
@@ -162,10 +178,14 @@
                                                     {#if article.image}
                                                         <img class="rounded-full"
                                                              src="{`data:image/png;base64,${article.image}`}"
+                                                             width="40"
+                                                             height="40"
                                                              alt="">
                                                     {:else}
                                                         <img class="rounded-full"
                                                              src="{`data:image/jpeg;base64,${defaultImage}`}"
+                                                             width="40"
+                                                             height="40"
                                                              alt="">
                                                     {/if}
                                                 </div>
@@ -193,8 +213,10 @@
                             <div class="m-auto">
                                 <div class="inline-block font-medium text-gray-800">
                                     {#if $page.params.page > 1}
-                                        <a class="ui-button border-black float-left p-2 no-underline" rel="external"
-                                           href={beforePage}>&laquo;</a>
+                                        <a class="ui-button border-black float-left p-2 no-underline"
+                                           rel="external"
+                                           href={beforePage}>&laquo;
+                                        </a>
                                     {:else}
                                         <div class="border-black float-left p-2 no-underline">&laquo;</div>
                                     {/if}
@@ -203,14 +225,18 @@
                                         {#if Number($page.params.page) === Number(pageNumber + 1)}
                                             <div class="border-black float-left p-2 underline">{(pageNumber + 1)}</div>
                                         {:else}
-                                            <a class="border-black float-left p-2 no-underline" rel="external"
-                                               href={'/articles/page/' + (pageNumber + 1) + hrefEndPaginator}>{(pageNumber + 1)}</a>
+                                            <a class="border-black float-left p-2 no-underline"
+                                               rel="external"
+                                               href={'/articles/page/' + (pageNumber + 1) + hrefEndPaginator}>{(pageNumber + 1)}
+                                            </a>
                                         {/if}
                                     {/each}
 
                                     {#if $page.params.page < maxPage}
-                                        <a class="ui-button border-black float-left p-2 no-underline" rel="external"
-                                           href={afterPage}>&raquo;</a>
+                                        <a class="ui-button border-black float-left p-2 no-underline"
+                                           rel="external"
+                                           href={afterPage}>&raquo;
+                                        </a>
                                     {:else}
                                         <div class="border-black float-left p-2 no-underline">&raquo;</div>
                                     {/if}
