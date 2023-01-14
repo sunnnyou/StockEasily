edd254968acf3e1d67835f98f6bb138be93438b1
Sunny Reitgassl <sunny.reitgassl@stud.th-rosenheim.de>
2023-01-14 22:36:03 +0100

    revert changes from previous commit because of lacking functionality add site /articles/qrcode add npm plugin qr-scanner

---
+++ StockEasily-UI/src/routes/articles/qrcode/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+import PageContent from "$components/common/PageContent.svelte";
+import PageCard from "$components/common/PageCard.svelte";
+import {t} from "$i18n/i18n.js";
+</script>
+
+<PageContent>
+    <PageCard title={$t('menu.qrCode')}>
+
+    </PageCard>
+</PageContent>
