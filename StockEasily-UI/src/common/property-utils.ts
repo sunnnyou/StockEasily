60e0218ce44c45252b234746401ad7ad23ad4327
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2023-01-14 19:57:09 +0100

    Reduce validatable article variable value assignment

--- StockEasily-UI/src/common/property-utils.ts
+++ StockEasily-UI/src/common/property-utils.ts
@@ -1,6 +1,5 @@
 import type {PropertyRequestDto, ValidatableProperty} from '../dto/property-request-dto';
 import type {ValidatableArticle} from '../dto/create-article-request-dto';
-import type {Validatable} from './validatable';
 
 function isEditingExistingProperty(article: ValidatableArticle, index: number): boolean {
     return index !== Number.NaN && article.properties.length > index;
@@ -14,12 +13,7 @@ export function onSaveProperty(article: ValidatableArticle, property: PropertyRe
         article.properties[index].value = property;
         return article.properties;
     }
-    const VALIDATABLE: ValidatableProperty = <Validatable<PropertyRequestDto> & { errors: { name: string; description: string } }>{
-        errors: {
-            description: '',
-            name: '',
-        }, value: property,
-    };
+    const VALIDATABLE: ValidatableProperty = {errors: {description: '', name: ''}, value: property};
     article.properties = [...article.properties, VALIDATABLE];
 
     return article.properties;
