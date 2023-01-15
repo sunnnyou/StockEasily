2bff92b19396379a8ee03bc00e4730fa007dcc38
Sunny Reitgassl <sunny.reitgassl@stud.th-rosenheim.de>
2023-01-15 01:54:13 +0100

    delete click_photo button add track to video add sleep function add stop that stops the while loop if the camera_button is pressed another time and sets inner_text of the button

--- StockEasily-UI/src/routes/articles/qrcode/+page.svelte
+++ StockEasily-UI/src/routes/articles/qrcode/+page.svelte
@@ -8,46 +8,55 @@
     import {goto} from "$app/navigation";
 
     onMount(() => {
-        // ask for camera permission
-
         let camera_button = document.getElementById("start-camera");
         let video = document.getElementById("video");
-        let click_button = document.getElementById("click_photo");
         let canvas = document.getElementById("canvas");
 
+        let stopScanning = true;
+
         camera_button.addEventListener('click', async function () {
+
+            stopScanning = !stopScanning;
+
             video.srcObject = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
                 .catch(error => {
                     console.log(error);
                     alert("Can't access camera. " + error.message);
                 });
+            while (!stopScanning) {
+                camera_button.innerText = "Stop Scanning";
+                canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+                let image_data_url = canvas.toDataURL('image/png');
+
+                // data url of the image
+                console.log(image_data_url);
+
+                QrScanner.scanImage(image_data_url)
+                    .then(result => {
+                        console.log(result);
+                        goto("/articles/" + result);
+                    })
+                    .catch(error => {
+                        console.log(error || 'No QR code found.');
+                    })
+                await sleep(200);
+            }
+            camera_button.innerText = "Start QR-Scan";
+
         });
 
-        click_button.addEventListener('click', function () {
-            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-            let image_data_url = canvas.toDataURL('image/png');
-
-            // data url of the image
-            console.log(image_data_url);
-
-            QrScanner.scanImage(image_data_url)
-                .then(result => {
-                    console.log(result);
-                    goto("/articles/" + result);
-                })
-                .catch(error => {
-                    console.log(error || 'No QR code found.');
-                    alert('No QR code found.');
-                });
-        });
+        function sleep(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms ?? 200));
+        }
     });
 </script>
 
 <PageContent>
     <PageCard title={$t('menu.qrCode')}>
-        <button id="start-camera">Start Camera</button>
-        <video id="video" width="320" height="240" autoplay></video>
-        <button id="click_photo">Click Photo</button>
-        <canvas id="canvas" width="320" height="240" hidden="hidden"></canvas>
+        <button id="start-camera">Start QR-Scan</button>
+        <video id="video" width="full" height="full" autoplay>
+            <track kind="captions" src="">
+        </video>
+        <canvas id="canvas" width="500" height="500" hidden="hidden"></canvas>
     </PageCard>
 </PageContent>
