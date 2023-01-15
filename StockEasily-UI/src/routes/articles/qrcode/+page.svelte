4d0058b5ae54564be449a14cf99e8fcff93b9b4a
Sunny Reitgassl <sunny.reitgassl@stud.th-rosenheim.de>
2023-01-15 01:00:10 +0100

    add error handling and hide canvas

--- StockEasily-UI/src/routes/articles/qrcode/+page.svelte
+++ StockEasily-UI/src/routes/articles/qrcode/+page.svelte
@@ -16,7 +16,11 @@
         let canvas = document.getElementById("canvas");
 
         camera_button.addEventListener('click', async function () {
-            video.srcObject = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+            video.srcObject = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+                .catch(error => {
+                    console.log(error);
+                    alert("Can't access camera. " + error.message);
+                });
         });
 
         click_button.addEventListener('click', function () {
@@ -30,9 +34,11 @@
                 .then(result => {
                     console.log(result);
                     goto("/articles/" + result);
-                }
-                )
-                .catch(error => console.log(error || 'No QR code found.'));
+                })
+                .catch(error => {
+                    console.log(error || 'No QR code found.');
+                    alert('No QR code found.');
+                });
         });
     });
 </script>
@@ -42,6 +48,6 @@
         <button id="start-camera">Start Camera</button>
         <video id="video" width="320" height="240" autoplay></video>
         <button id="click_photo">Click Photo</button>
-        <canvas id="canvas" width="320" height="240"></canvas>
+        <canvas id="canvas" width="320" height="240" hidden="hidden"></canvas>
     </PageCard>
 </PageContent>
