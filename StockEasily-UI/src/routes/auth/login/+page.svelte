7d945dfa5775b36d164f336d9f665f2008be99c0
Leon Vauth <leon.vauth@stud.th-rosenheim.de>
2023-01-16 21:00:40 +0100

    User menu icon fix, login register page

---
+++ StockEasily-UI/src/routes/auth/login/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import type { ActionData} from '../../../.svelte-kit/types/src/routes'
+    import {applyAction, enhance} from "$app/forms";
+    import {invalidateAll} from "$app/navigation";
+    export let form: ActionData
+</script>
+
+<h1>Login</h1>
+
+<form action="?/login" method="POST" use:enhance={() => {
+    return async ({result}) => {
+        invalidateAll()
+        await applyAction(result)
+    }
+}}>
+    <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required />
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="text" name="password" id="password" required />
+    </div>
+
+    {#if form?.invalid}
+        <p class="error">Username or password required.</p>
+    {/if}
+
+    {#if form?.credentials}
+        <p class="error">Wrong username or password credentials.</p>
+    {/if}
+
+    <button type="submit">Login</button>
+</form>
---
+++ StockEasily-UI/src/routes/auth/register/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { ActionData} from './$types';
+
+    import {enhance} from '$app/forms'
+    export let form: ActionData
+</script>
+
+<h1>Register</h1>
+
+<form action="?/register" method="POST" use:enhance>
+    <div>
+        <label for="username">Username</label>
+        <input name="username" id="username" type="text" required>
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input name="username" id="password" type="text" required>
+    </div>
+
+
+    {#if form?.user}
+        <p class="error">Username is taken.</p>
+    {/if}
+    <button type="submit">Register</button>
+</form>
