42b68065245e961c356f2a79b25c4fd13a550fd0
Daniel Hentzschel <daniel.hentzschel@stud.th-rosenheim.de>
2023-01-16 21:54:15 +0100

    Do not allow duplicate properties

--- StockEasily-UI/src/common/property-utils.ts
+++ StockEasily-UI/src/common/property-utils.ts
@@ -6,6 +6,10 @@ function isEditingExistingProperty(article: ValidatableArticle, index: number): 
     return index !== Number.NaN && article.properties.length > index;
 }
 
+function isDuplicateProperty(properties: ValidatableProperty[], property: PropertyRequestDto) {
+    return properties.some(p => p.value.name === property.name && p.value.description === property.description);
+}
+
 export function onSaveProperty(article: ValidatableArticle, property: PropertyRequestDto, index: number = Number.NaN): ValidatableProperty[] {
     property.name = property.name?.trim() || '';
     property.description = property.description?.trim() || '';
@@ -14,6 +18,12 @@ export function onSaveProperty(article: ValidatableArticle, property: PropertyRe
         article.properties[index].value = property;
         return article.properties;
     }
+
+    if (isDuplicateProperty(article.properties, property)) {
+        console.debug('Property already exists');
+        return article.properties;
+    }
+
     const VALIDATABLE: ValidatableProperty = {errors: {description: '', name: ''}, value: property};
     article.properties = [...article.properties, VALIDATABLE];
 
